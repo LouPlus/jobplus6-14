193d1355a384eb2b6d65cd4d34038acca99c39ad
vision <vision4fun@gmail.com>
2018-05-23 22:41:48 +0800

    resolved #6,#7,#9 (#30)

---
+++ jobplus/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_migrate import Migrate
+from .config import configs
+from .models import db
+
+
+def reg_bps(app):
+    from .handlers import front, admin, user, job, company
+    app.register_blueprint(front)
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+    app.register_blueprint(job)
+    app.register_blueprint(company)
+
+
+def reg_exts(app):
+
+    db.init_app(app)
+    Migrate(app, db)
+
+
+def create_app(config):
+
+    app = Flask(__name__)
+    app.config.from_object(configs.get(config))
+
+    reg_bps(app)
+    reg_bps(app)
+
+    return app
