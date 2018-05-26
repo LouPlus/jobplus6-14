45215e4b54bb08f85a62e9df4864c5b6e7c99188
vision <vision@VisiondeMacBook-Pro.local>
2018-05-27 00:32:40 +0800

    resolved #8

--- jobplus/config.py
+++ jobplus/config.py
@@ -1,12 +1,15 @@
 class BaseConfig(object):
     """ 配置基类 """
     SECRET_KEY = 'jobplus-6-14'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    INDEX_PER_PAGE = 9
+    ADMIN_PER_PAGE = 15
 
 
 class DevelopmentConfig(BaseConfig):
     """ 开发环境配置 """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://jobplus:jobplus@localhost:3306/jobplus?charset=utf8'
 
 
 class ProductionConfig(BaseConfig):
